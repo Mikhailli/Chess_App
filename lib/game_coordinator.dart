6d30569d48d19bd95efe338c17f2ce7de26f611a
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-10 15:32:29 +0700

    refactor(game_coordinator): merge black and white pieces collection

--- lib/game_coordinator.dart
+++ lib/game_coordinator.dart
@@ -3,30 +3,35 @@ import 'package:chess/pieces/chess_piece.dart';
 import 'package:collection/collection.dart';
 
 class GameCoordinator {
-  final List<ChessPiece> whitePieces;
-  final List<ChessPiece> blackPieces;
+  //final List<ChessPiece> whitePieces;
+  //final List<ChessPiece> blackPieces;
 
-  List<ChessPiece> get pieces => [
-    ...whitePieces,
-    ...blackPieces
-  ].toList();
+
+  final List<ChessPiece> pieces;
 
   PlayerColor currentTurn = PlayerColor.white;
 
-  GameCoordinator(this.whitePieces, this.blackPieces);
+  //GameCoordinator(this.whitePieces, this.blackPieces, this.pieces);
+  GameCoordinator(this.pieces);
 
   ChessPiece? pieceOfTile(int x, int y) => pieces.firstWhereOrNull((p) => p.x == x && p.y == y);
 
   factory GameCoordinator.newGame() {
     return GameCoordinator(
+        //[
+        //  Bishop(PlayerColor.white, Location(2, 0)),
+        //  Bishop(PlayerColor.white, Location(5, 0))
+        //],
+        //[
+        //  Bishop(PlayerColor.black, Location(2, 7)),
+        //  Bishop(PlayerColor.black, Location(5, 7))
+        //],
         [
           Bishop(PlayerColor.white, Location(2, 0)),
-          Bishop(PlayerColor.white, Location(5, 0))
-        ],
-        [
+          Bishop(PlayerColor.white, Location(5, 0)),
           Bishop(PlayerColor.black, Location(2, 7)),
           Bishop(PlayerColor.black, Location(5, 7))
-        ],
+        ]
     );
   }
 }
